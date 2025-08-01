e50dcc2449dd914cf1bd6c3b16f69a7138694937
Exauce <exauceaniti@gmail.com>
2025-08-01 12:29:48 +0100

    Exercices sur plusieurs fnctions

--- demo.php
+++ demo.php
@@ -1,12 +1,44 @@
 <?php
 
-function bonjour($nom = null)
+function repondre_oui_nom($phrase)
 {
-    if ($nom === null) {
-        return "Bonjour \n";
-    } else {
-        return "Bonjour" . " " . $nom . "! \n";
+    while (true) {
+        $reponse = readline($phrase . " o(oui) ou n(nom) ");
+        if ($reponse === 'o') {
+            return true;
+        } else if ($reponse === 'n') {
+            return false;
+        } else {
+            echo "Reponse Invalide. Veillez repondre par o(oui) ou n(non). \n";
+        }
     }
 }
-$salutation = bonjour('Exau');
-echo $salutation;
+
+function demander_un_creneau($phrase = "veillez entrer un creneau")
+{
+    echo $phrase . "\n";
+    while (true) {
+        $debut = (int)readline('Entrer une heur de debut : ');
+        if ($debut >= 0 || $debut <= 23) {
+            break;
+        }
+    }
+    while (true) {
+        $fin = (int)readline("Entrer l\'heur de fermeture : ");
+        if ($fin >= 0 && $fin <= 23 && $fin > $debut) {
+            break;
+        }
+    }
+    return [$debut, $fin];
+}
+
+function demander_un_creneaux($mot = "Entrer un crenneaux")
+{
+    $creneaux = [];
+    $continuer = true;
+    while ($continuer) {
+        $creneaux[] = demander_un_creneau();
+        $continuer = repondre_oui_nom("Voulez vous continuer ?");
+    }
+    return $creneaux;
+}
